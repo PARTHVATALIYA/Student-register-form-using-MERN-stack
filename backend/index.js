672245a177fe123bd6d9352e838d5e130894a440
parth <parth>
2023-02-11 22:13:36 +0530

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
 
-const app = express()
+const app =express()
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
